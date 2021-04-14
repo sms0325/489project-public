156e1c9a97a4369a570e4f93666f1f949e3d46a6
sms0325 <sms0325@tamu.edu>
2021-04-13 22:17:50 -0500

    Set up a skeleton for the models.py stuff - will still need to add views

--- recommender/models.py
+++ recommender/models.py
@@ -1,4 +1,22 @@
 from django.db import models
 
+class Paper(models.Model):
+    title = models.CharField(max_length=500) #name of paper
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
+
+class Researcher(models.Model):
+    name = models.CharField(max_length=100) #name of researcher
+    papers = models.ManyToManyField(Paper) #connecting back to their papers
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
 
 # Create your models here.
